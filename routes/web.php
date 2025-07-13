9cbee646c58ee843fdf6702e0ae265d1188617e5
fanntt <160685274+fanntt@users.noreply.github.com>
2025-07-13 17:05:04 +0700

    model, controller, middleware, dan auth

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route khusus admin
+Route::middleware(['role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return 'Halaman Admin';
+    });
+});
+
+// Route untuk admin dan petugas
+Route::middleware(['role:admin,petugas'])->group(function () {
+    Route::get('/petugas', function () {
+        return 'Halaman Petugas/Admin';
+    });
+});
